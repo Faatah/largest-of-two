4565f51fd7ffdb8adb674504762e0043072d68b3
Faatah <guure781@gmail.com>
2024-12-21 15:09:24 +0300

    done exercise for largest two number

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let num1=50
+let num2=100
+
+if(num1>num2){
+    console.log("The largest number out of the two numbers is "+num1)
+}else if(num1<num2){
+    console.log("The largest number out of the two numbers is "+num2)
+}
